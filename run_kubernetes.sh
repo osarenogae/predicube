2b7ae4026be89245d355dcec28a3cbffcbddb573
EC2 Default User <ec2-user@ip-172-31-39-84.ec2.internal>
2023-04-10 13:10:55 +0000

    project upload

---
+++ run_kubernetes.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath=ochuko/predicube
+
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run predicube\
+    --image=$dockerpath\
+    --port=80 --labels app=predicube
+
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward predicube 8000:80
